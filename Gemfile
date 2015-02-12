4b765247044cb078a2d0d2bd78f833c1e5ef23dd
Emma Borhanian <emma.borhanian@gmail.com>
2015-02-11 19:17:26 -0800

    add pry

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'http://rubygems.org'
 
 gem 'rspec'
 gem 'guard-rspec'
+
+gem 'pry'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require 'pry'
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each do |file|
   require file
