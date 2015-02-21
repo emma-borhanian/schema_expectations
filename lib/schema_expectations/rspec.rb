1d858326996fd18d9ae0c290ca030d13773650f1
Emma Borhanian <emma.borhanian@gmail.com>
2015-02-21 01:03:59 -0800

    remove the rspec-rails model restriction

--- lib/schema_expectations/rspec.rb
+++ lib/schema_expectations/rspec.rb
@@ -2,5 +2,5 @@ require 'rspec/core'
 require 'schema_expectations/rspec_matchers'
 
 RSpec.configure do |config|
-  config.include SchemaExpectations::RSpecMatchers, type: :model
+  config.include SchemaExpectations::RSpecMatchers
 end
